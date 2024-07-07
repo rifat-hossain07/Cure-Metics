98f0e64948ab74201780bb1fa53b306e4c935f5d
Rifat Hossain <rifatrabby78@gmail.com>
2024-07-07 14:28:24 +0600

    setup done

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1 className="text-9xl">Cure Metics</h1>
+    </div>
+  );
+}
